ec92df826db491c18e3110c37fdfbd64a3f55892
TEJASWI GINJUPALLI <2100032434@kluniversity.in>
2022-08-03 16:05:06 +0530

    This is my initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+# a = 3
+# b = 2.65
+# d = 2+5j
+# print ("int is",a)
+# print ("float is",b)
+# print ("complex is",d)
+
+# a=True
+# b=False
+# print(a)
+# print(b)
+
+# str="TEJASWI"
+# print(str)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str*2)
+
+
+# i=1
+# while i<=3:
+#     print(i)
+#     i+=1
+#     print("END")
+
+# for  i in range(1,4):
+#       print (i)
+# 	  print ("END“)
+
+# import random
+# n=random.randbytes(3)
+# print(n)
+# print(random.randrange(1,8))
+# print(random.randint(100,211))
+# mylist=["jadeja","ashwin","rahmane","shami","dhoni","virat"]
+# mylist1=["jadeja","ashwin","rahmane","shami","dhoni","virat"]
+# print(random.choice(mylist))
+# #print(random.choice(mylist1))
+# random.shuffle(mylist)
+
+# import re
+# txt = "Use of python is Machine Learning!"
+# x = re.search("^Use.*Learning$", txt)
+# if (x):
+#     ("Yes! We have a match!")
+# else:
+#     print("Yes match")
+
+# #Print a list of all matches ("in") from a text
+# import re
+# txt="Use of python in Machine Learning"
+# x= re.findall("in", txt)
+# print(x)
+
+# #** SEARCH ** PROGRAM
+# import re
+# txt = "Python is on of the most popular languages"
+# searchobj = re.search("\s",txt)
+# print("the first white-space character is located")
+
+

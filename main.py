54ecdd98f1aaaed1ec5413cd6a1b56644b757a4a
Teja201 <2100032434@kluniversity.in>
2022-08-08 11:11:59 +0530

    first commit

--- main.py
+++ main.py
@@ -60,3 +60,10 @@
 # print("the first white-space character is located")
 
 
+# import pytest
+#
+# @pytest.mark.parametrize("num, output",[(1,11),(2,22),(3,35),(4,44)])
+# def test_multiplication_11(num, output):
+#    assert 11*num == output
+
+
